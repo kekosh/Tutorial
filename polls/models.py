f4e35e43fdd3236d97c091672faf810c4c835b01
kekosh <kenta0540@gmail.com>
2018-05-17 13:44:57 +0000

    Tutorial モデルを有効にするの前まで

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    """
+    ・max_lengthはCharFieldを使用する際必須
+    ・第一引数で人間に分かりやすい名前を設定
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+class Choice(models.Model):
+    """
+    ForeignKeyによるリレーションの設定
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
