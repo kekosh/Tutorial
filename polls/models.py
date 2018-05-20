31283950d7c8a02c46dd3b27443ff4128c1c4f83
kekosh <kenta0540@gmail.com>
2018-05-20 13:32:25 +0000

    try use Django API

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+#カスタムメソッド用
+import datetime
+from django.utils import timezone
+
+
 # Create your models here.
 class Question(models.Model):
     """
@@ -9,6 +14,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
+    #オブジェクトを参照した時の表記を指定
+    def __str__(self):
+        return self.question_text
+        
+    #カスタムメソッド
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 class Choice(models.Model):
     """
     ForeignKeyによるリレーションの設定
@@ -16,4 +29,11 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    #オブジェクトを参照した時の表記を指定
+    def __str__(self):
+        return self.choice_text
+
+
+    
 
