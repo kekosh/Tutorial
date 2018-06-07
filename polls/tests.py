c449bbdd4bc40d1ad941ba5bfa0246f7ea3bc6ec
kekosh <kenta0540@gmail.com>
2018-06-07 13:59:14 +0000

    Tutorial5 Phase ViewTest at NewWindow

--- polls/tests.py
+++ polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -21,3 +22,7 @@ class QuestionModelTests(TestCase):
         seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
+from django.utils import timezone
+
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
@@ -11,7 +13,10 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         """ return the last question data from database """
-        return Question.objects.order_by('pub_date')[:5]
+        """return Question.objects.order_by('pub_date')[:5]"""
+        
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()).order_by('pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
