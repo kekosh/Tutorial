31a1744a609ac677af661b9b17d2eb7fc1ef3157
kekosh <kenta0540@gmail.com>
2018-06-10 13:29:17 +0000

    Tutorial7. end phrase of add relational objects

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+"""admin.site.register(Question)
+"""
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information', {'fields':['pub_date'], 'classes':['collapse']})
+    ]
+    inlines = [ChoiceInline]
+
+"""
+モデルのadminオプションを変更したい場合は、モデル毎にadminクラスを作成し、
+registerの第2引数として渡す
+"""
+admin.site.register(Question, QuestionAdmin)
