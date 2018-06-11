d863d50980275a607ab2221089138763b4f38deb
kekosh <kenta0540@gmail.com>
2018-06-11 14:02:10 +0000

    complete introduction Tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -11,12 +11,16 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
     
 class QuestionAdmin(admin.ModelAdmin):
+    """ 
     fieldsets = [
         (None, {'fields':['question_text']}),
         ('Date information', {'fields':['pub_date'], 'classes':['collapse']})
     ]
     inlines = [ChoiceInline]
-
+    """
+    list_display = ('question_text','pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 """
 モデルのadminオプションを変更したい場合は、モデル毎にadminクラスを作成し、
 registerの第2引数として渡す
